5282e4034154e84b139a9503f1ce6fed4089e521
thakuramit79 <thakuramit79@gmail.com>
2025-05-28 15:03:31 +0530

    Add export to EquipmentManagement component

---
+++ src/pages/Deals.tsx
@@ -0,0 +1,9 @@
+// Update the formatCurrency function in Deals.tsx
+const formatCurrency = (value: number) => {
+  return new Intl.NumberFormat('en-IN', {
+    style: 'currency',
+    currency: 'INR',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(value);
+};
