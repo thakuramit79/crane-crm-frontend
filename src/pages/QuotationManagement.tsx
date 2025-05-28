a23d272fffbde38df47c8fe247f696b71ab08549
thakuramit79 <thakuramit79@gmail.com>
2025-05-28 15:03:41 +0530

    Update currency formatting to INR

--- src/pages/QuotationManagement.tsx
+++
@@ -1,9 +0,0 @@
-// Update the formatCurrency function in QuotationManagement.tsx
-const formatCurrency = (amount: number) => {
-  return new Intl.NumberFormat('en-IN', {
-    style: 'currency',
-    currency: 'INR',
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
-  }).format(amount);
-};
