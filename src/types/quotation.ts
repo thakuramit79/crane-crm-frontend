0fa85ee6890cfaed182958e25cc1321a8113e2fe
thakuramit79 <thakuramit79@gmail.com>
2025-05-29 09:48:04 +0530

    Fix Toast component exports and usage

--- src/types/quotation.ts
+++ src/types/quotation.ts
@@ -1,47 +1,24 @@
 export interface QuotationInputs {
-  orderType: string;
-  machineType: string;
-  workingHours: number;
-  dayNight: string;
-  shift: string;
-  sundayWorking: string;
-  foodResources: number;
-  accomResources: number;
-  usage: string;
-  siteDistance: number;
-  trailerCost: number;
-  mobRelaxation: string;
-  workingCost: number;
-  elongation: number;
-  dealType: string;
-  extraCharge: number;
-  billing: string;
-  riskFactor: string;
-  incidentalCharges: number;
-  otherFactors: string;
-  otherFactorsCharge: number;
-}
-
-export interface QuotationCalculations {
   baseRate: number;
-  totalHours: number;
-  workingCost: number;
-  elongationCost: number;
-  trailerCost: number;
-  foodAccomCost: number;
-  usageLoadFactor: number;
-  extraCharges: number;
-  riskAdjustment: number;
-  gstAmount: number;
-  totalAmount: number;
+  workingHours: number;
+  rentalDays: number;
+  foodCharge: number;
+  accomCharge: number;
+  numResources: number;
+  usagePercent: number;
+  elongationPercent: number;
+  commercialCharge: number;
+  riskPercent: number;
+  incidentalCharge: number;
+  otherCharge: number;
 }
 
 export interface Quotation extends QuotationInputs {
   id: string;
   leadId: string;
+  totalRent: number;
   version: number;
-  createdBy: string;
   createdAt: string;
   updatedAt: string;
-  calculations: QuotationCalculations;
+  createdBy: string;
 }
