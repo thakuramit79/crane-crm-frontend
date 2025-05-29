b18bdf295c8ccbad31b25cadbbd535e4dd2ca6ed
thakuramit79 <thakuramit79@gmail.com>
2025-05-29 10:44:57 +0530

    Update Quotation Service and Form Integration

--- src/types/quotation.ts
+++ src/types/quotation.ts
@@ -1,24 +1,47 @@
 export interface QuotationInputs {
-  baseRate: number;
+  orderType: string;
+  machineType: string;
   workingHours: number;
-  rentalDays: number;
-  foodCharge: number;
-  accomCharge: number;
-  numResources: number;
-  usagePercent: number;
-  elongationPercent: number;
-  commercialCharge: number;
-  riskPercent: number;
-  incidentalCharge: number;
-  otherCharge: number;
+  dayNight: string;
+  shift: string;
+  sundayWorking: string;
+  foodResources: number;
+  accomResources: number;
+  usage: string;
+  siteDistance: number;
+  trailerCost: number;
+  mobRelaxation: string;
+  workingCost: number;
+  elongation: number;
+  dealType: string;
+  extraCharge: number;
+  billing: string;
+  riskFactor: string;
+  incidentalCharges: number;
+  otherFactors: string;
+  otherFactorsCharge: number;
+}
+
+export interface QuotationCalculations {
+  baseRate: number;
+  totalHours: number;
+  workingCost: number;
+  elongationCost: number;
+  trailerCost: number;
+  foodAccomCost: number;
+  usageLoadFactor: number;
+  extraCharges: number;
+  riskAdjustment: number;
+  gstAmount: number;
+  totalAmount: number;
 }
 
 export interface Quotation extends QuotationInputs {
   id: string;
   leadId: string;
-  totalRent: number;
   version: number;
+  createdBy: string;
   createdAt: string;
   updatedAt: string;
-  createdBy: string;
+  calculations: QuotationCalculations;
 }
